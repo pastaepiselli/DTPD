482238d8bfe8a251c2e139e6e7b2b0732caea889
pastaepiselli <rossilorenzo371@gmail.com>
2025-11-07 17:11:45 +0100

    add genere to EnumType

--- myDataTypes/EnumType.py
+++ myDataTypes/EnumType.py
@@ -1,7 +1,11 @@
-from enum import *
+from enum import StrEnum, auto
 
 class StatoOrdine(StrEnum):
     in_preparazione = auto()
     inviato = auto()
     da_saldare = auto()
     saldato = auto()
+
+class Genere(StrEnum):
+    donna = auto()
+    uomo = auto()
