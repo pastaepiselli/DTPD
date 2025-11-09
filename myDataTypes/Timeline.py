b4e1e86649d4f62c86c0543a9e42d9a5f2a6faba
pastaepiselli <rossilorenzo371@gmail.com>
2025-11-09 23:57:51 +0100

    11th commit

--- myDataTypes/Timeline.py
+++ myDataTypes/Timeline.py
@@ -1,6 +1,10 @@
 from datetime import datetime, date
 
 class TimeRange(date): 
-    def __new__(cls, year, month, day):
-        return super().__new__(cls,year, month, day)
+    def __new__(cls, start: datetime, end: datetime):
+        # non so che cosa ho scritto
+        if end < start:
+            return ValueError("Start date cant be higher than end date")
+        return super().__new__(cls, start, end)
+        
 
